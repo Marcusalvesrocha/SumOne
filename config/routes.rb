dbb7b6b445cdf4e8518577c225d744b25d292b4c
Marcus Rocha <sif.marcusvinicius@gmail.com>
2017-09-29 01:17:50 +0000

    Ajustando Administrativo

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   root 'site/home#index'
   
   namespace :site do
-    get 'home/index'
+    get 'home', to: 'home#index'
   end
 
   namespace :administrative do
-    get 'dashboard/index'
+    get 'dashboard', to: 'dashboard#index'
   end
 
   devise_for :admins
